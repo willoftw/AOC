51e0ae2002b880f11cdfbdc6aa6a225d42e9a291
Will <will.olner@assetinsights.co.uk>
2020-12-02 11:02:39 +0000

    solution

---
+++ AOC1/AOC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC1
+{
+    public class AOC
+    {
+        public int[] GetSummableValues(List<int> values, int targetSum)
+        {
+            List<int> ret = new List<int>();
+            values.ForEach(x => values.Contains(x-2020));
+            var t = values.Where(val => values.Contains(targetSum - val)).Select(val => val).ToList();
+            return t.ToArray();
+        }
+
+        public int MultiplySummableValues(List<int> values, int targetSum)
+        {
+            int[] summableValues = GetSummableValues(values, targetSum);
+            return summableValues[0] * summableValues[1];
+        }
+    }
+}
