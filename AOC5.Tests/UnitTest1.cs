0f61e7248f11640c3ba5adf510966014b6aa15e2
Will <will.olner@assetinsights.co.uk>
2020-12-06 19:25:09 +0000

    working p1

--- AOC5.Tests/UnitTest1.cs
+++ AOC5.Tests/UnitTest1.cs
@@ -1,20 +1,82 @@
 using System;
 using Xunit;
 using AOC5;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AOC5.Tests
 {
     public class UnitTest1
     {
+        List<string> input = new List<string>();
+        List<int> seatids = new List<int>();
+        public UnitTest1()
+        {
+              StreamReader reader = File.OpenText("data.txt");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                input.Add(line);
+            }
+            reader.Close();
+        }
+        //dotnet test --filter "AOC5.Tests.UnitTest1"
         [Fact]
-        public void ShouldReturnCorrectSeat()
+        public void ShouldReturnCorrectSeatFBFBBFFRLR()
         {
             using(AOC aoc = new AOC())
             {
                 //[44,5] == row 44 column 5
-                int[] seat = new int[] {44,5};
+                int[] seat = new int[] {44,5,357};
                 Assert.Equal(seat, aoc.Decode("FBFBBFFRLR"));
             }
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectSeatBFFFBBFRRR()
+        {
+            using(AOC aoc = new AOC())
+            {
+                //[44,5] == row 44 column 5
+                int[] seat = new int[] {70,7,567};
+                Assert.Equal(seat, aoc.Decode("BFFFBBFRRR"));
+            }
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectSeatFFFBBBFRRR()
+        {
+            using(AOC aoc = new AOC())
+            {
+                //[44,5] == row 44 column 5
+                int[] seat = new int[] {14,7,119};
+                Assert.Equal(seat, aoc.Decode("FFFBBBFRRR"));
+            }
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectSeatBBFFBBFRLL()
+        {
+            using(AOC aoc = new AOC())
+            {
+                //[44,5] == row 44 column 5
+                int[] seat = new int[] {102,4,820};
+                Assert.Equal(seat, aoc.Decode("BBFFBBFRLL"));
+            }
+        }
+
+        [Fact]
+        public void ShouldCalculateHighestSeatID()
+        {
+            AOC aoc = new AOC();
+            
+            foreach(string s in input)
+            {
+                seatids.Add(aoc.Decode(s)[2]);
+            }
+            Assert.Equal(926, seatids.Max());
+            
         }
     }
 }
--- AOC5/AOC.cs
+++ AOC5/AOC.cs
@@ -13,11 +13,59 @@ namespace AOC5
         {
             
         }
-
-        public IEnumerable<int> Decode(string v)
+        //F means "front", B means "back", L means "left", and R means "right".
+        //Seats are 0-127
+        //first 7 characters
+        public int[] Decode(string v)
         {
-            int[] seat = new int[] {44,5};
-            return seat;
+            int[] seat = new int[] {0,0,0};
+            char[] chars = v.ToCharArray();
+            int[] rowrange = new int[] {0,128} ;
+            //32,64
+
+            //front = min = rowrange[0]+(Range(rowrage)/2); max = rowrage[1]
+            for(int i = 0; i<7 ; i++)
+            {
+                int r = Range(rowrange);
+                Console.WriteLine(chars[i]);
+                switch(chars[i])
+                {
+                    case 'F':
+                        rowrange[0] = rowrange[0];
+                        rowrange[1] = rowrange[0]+(r/2);
+                        break;
+                    case 'B':
+                        rowrange[0] = rowrange[0]+r/2;
+                        rowrange[1] = rowrange[1];
+                        break;
+                }
+                Console.WriteLine($" row {rowrange[0]},{rowrange[1]}");
+            }
+            int[] colrange = new int[] {0,7} ;
+            for(int i = 7; i<10 ; i++)
+            {
+                int r = Range(colrange);
+                Console.WriteLine(chars[i]);
+                switch(chars[i])
+                {
+                    case 'L':
+                        colrange[0] = colrange[0];
+                        colrange[1] = colrange[0]+(r/2);
+                        break;
+                    case 'R':
+                        colrange[0] = colrange[0]+r/2;
+                        colrange[1] = colrange[1];
+                        break;
+                }
+                Console.WriteLine($" col {colrange[0]},{colrange[1]}");
+            }
+            
+            return new int[] {rowrange[1]-1,colrange[1],(rowrange[1]-1)*8+colrange[1]};
+        }
+
+        private int Range(int[] range)
+        {
+            return range[1]-range[0];
         }
     }
 }
