afb8b2c5f0edb144962511febe067b13e1500eb5
Will <will.olner@assetinsights.co.uk>
2020-12-09 09:11:37 +0000

    working D9P1 answer

--- AOC9.Tests/UnitTest1.cs
+++ AOC9.Tests/UnitTest1.cs
@@ -1,14 +1,60 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using Xunit;
 
 namespace AOC9.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        public List<string> ParseInput(string filename)
         {
+            List<string> ret = new List<string>();
+            StreamReader reader = File.OpenText(filename);
+            string line = "";
+            while ((line = reader.ReadLine()) != null)
+            {
+                ret.Add(line);
+            }
+            reader.Close();
 
+            return ret;
+        }
+
+        [Theory]
+        [InlineData("data.txt", 20)]
+        //[InlineData("data_2.txt",660)]
+        public void ShouldParseDataCorrectly(string datafile, int result)
+        {
+            List<string> input = ParseInput(datafile);
+            Assert.Equal(result,input.Count);
+        }
+
+        [Theory]
+        [InlineData("data.txt", 20)]
+        //[InlineData("data_2.txt",660)]
+        public void ShouldProcessDataCorrectly(string datafile, int result)
+        {
+            using (AOC aoc = new AOC())
+            {
+                List<string> input = ParseInput(datafile);
+                List<double> data = aoc.ProcessData(input);
+                Assert.Equal(result,data.Count);
+            }
+        }
+
+        [Theory]
+        [InlineData("data.txt", 5,127)]
+        [InlineData("data_2.txt", 25,556543474)]
+        //[InlineData("data_2.txt",660)]
+        public void ShouldGetNotSummableValueCorrectly(string datafile, int preamble, int result)
+        {
+            using (AOC aoc = new AOC())
+            {
+                List<string> input = ParseInput(datafile);
+                List<double> data = aoc.ProcessData(input);
+                Assert.Equal(result,aoc.GetNotSummableValue(data,preamble));
+            }
         }
     }
 }
--- AOC9/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AOC9
-{
-    public class Class1
-    {
-    }
-}
